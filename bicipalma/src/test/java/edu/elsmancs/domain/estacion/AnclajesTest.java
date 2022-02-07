bb28f570774015fc6d6c7b6c152c2b0d5691a1f1
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-07 17:47:12 +0100

    test(isAnclajeOcupado): add method to test if Anclaje position is anchor

--- bicipalma/src/test/java/edu/elsmancs/domain/estacion/AnclajesTest.java
+++ bicipalma/src/test/java/edu/elsmancs/domain/estacion/AnclajesTest.java
@@ -1,5 +1,7 @@
 package edu.elsmancs.domain.estacion;
 import static org.junit.Assert.*;
+
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
 
 import edu.elsmancs.domain.bicicleta.Bicicleta;
@@ -10,6 +12,22 @@ public class AnclajesTest {
     public void espaciosAnclajes() {
         Anclajes anclajes = new Anclajes(3);
         assertEquals(3, anclajes.numAnclajes());
-    }    
-    
+    }   
+
+    @Test
+    public void isAnclajeOcupado() {
+        Anclajes anclajes = new Anclajes(3);
+        Bicicleta bici = new Bicicleta(1);
+        Bicicleta bici2 = new Bicicleta(1);
+
+        
+        anclajes.ocuparAnclaje(1, bici);
+        anclajes.ocuparAnclaje(2, bici2);
+
+        assertEquals(true, anclajes.isAnclajeOcupado(1));
+        assertEquals(true, anclajes.isAnclajeOcupado(2));
+
+
+        
+    }
 }
