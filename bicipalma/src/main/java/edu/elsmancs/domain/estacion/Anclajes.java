012a3d2f981a61097809d356a7a31d12ed14b019
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-07 17:26:44 +0100

    feat(crearAnclajes): add method to asign an object to empty array spaces

---
+++ bicipalma/src/main/java/edu/elsmancs/domain/estacion/Anclajes.java
@@ -0,0 +1,19 @@
+package edu.elsmancs.domain.estacion;
+
+public class Anclajes {
+
+    private final Anclaje[] anclajes;
+
+    Anclajes(int anclajesTotales) {
+        this.anclajes = new Anclaje[anclajesTotales];
+        crearAnclajes();
+    }
+
+    private void crearAnclajes() {
+        for (int i = 0; i < anclajes.length; i++) {
+            this.anclajes[i] = new Anclaje();
+        }
+    }
+
+    
+}
