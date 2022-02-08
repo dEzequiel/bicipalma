49ed9097ede154929f5ce9c5ae5c324cd4f666cd
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-08 15:59:45 +0100

    test(checkTarjetaUsuario, cretirarBicicleta): add test to check if tarjetaUsuario is deactivate, add; add test to check if anchor is free after retirarBicicleta

--- bicipalma/src/test/java/edu/poniperro/domain/estacion/EstacionTest.java
+++ bicipalma/src/test/java/edu/poniperro/domain/estacion/EstacionTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import edu.poniperro.domain.bicicleta.Bicicleta;
 import edu.poniperro.domain.estacion.Anclajes;
 import edu.poniperro.domain.estacion.Estacion;
+import edu.poniperro.domain.tarjetaUsuario.TarjetaUsuario;
 
 public class EstacionTest {
 
@@ -29,5 +30,29 @@ public class EstacionTest {
         estacion.anclarBicicleta(bici);
 
         assertEquals(2, estacion.anclajesLibres());
+    }
+
+    @Test
+    public void checkTarjetaUsuario() {
+        TarjetaUsuario tarjetaUsuario = new TarjetaUsuario("Eze", false);
+        Estacion estacion = new Estacion(1, "Manacor", 3);
+        assertEquals(false, estacion.leerTarjetaUsuario(tarjetaUsuario));
+    }
+
+    @Test
+    public void retirarBicicleta() {
+        TarjetaUsuario tarjetaUsuario = new TarjetaUsuario("Eze", true);
+        Estacion estacion = new Estacion(1, "Manacor", 3);
+        Bicicleta bici = new Bicicleta(1);
+        Bicicleta bici2 = new Bicicleta(2);
+        estacion.anclarBicicleta(bici);
+        estacion.anclarBicicleta(bici2);
+
+        assertEquals(1, estacion.anclajesLibres());
+
+        estacion.retirarBicicleta(tarjetaUsuario);
+        assertEquals(2, estacion.anclajesLibres());
+
+
     }
 }
